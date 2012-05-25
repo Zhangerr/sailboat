5f3f0436b9c798f0c74096c2f14cdb2447053fc4
alex zhang <alexzhangh@gmail.com>
2012-05-24 23:47:35 -0700

    compile with 'g++ -o test test.cpp -l boost_filesystem'

--- test.cpp
+++ test.cpp
@@ -1,14 +1,54 @@
-#include <boost/lambda/lambda.hpp>
 #include <iostream>
-#include <iterator>
-#include <algorithm>
-
+#include "boost/asio.hpp"
+#include <string>
+#include <ctime>
+#include <fstream>
 using namespace std;
+using boost::asio::ip::tcp;
+
+string make_daytime_string()
+{
+  time_t now = time(0);
+  return ctime(&now);
+}
+
+string getindex() {
+	string result;
+	string line;
+	ifstream ind("index.html");
+	if(ind.is_open()) {
+		while(ind.good()) {
+			getline(ind,line);
+			result += line + "\r\n";
+		}
+		return result;
+	} else {
+		return "could not find index";
+	}
+}
+//need to include content-length
 int main()
 {
-    using namespace boost::lambda;
+   /* using namespace boost::lambda;
     typedef std::istream_iterator<int> in;
 	cout << "enter numbers" << endl;
     std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );*/
+    boost::asio::io_service is;
+    tcp::acceptor acceptor(is, tcp::endpoint(tcp::v4(), 80)); //because port 80, must run as sudo
+    
+    while(true) {
+		tcp::socket sock(is);
+		acceptor.accept(sock);
+		boost::system::error_code err;
+		//cout << make_daytime_string() << endl;
+		boost::asio::write(sock, boost::asio::buffer("\
+HTTP/1.1 200 OK\r\n\
+Date: " + make_daytime_string() + " GMT\r\n\
+Server: sailboat\r\n\
+Connection: close\r\n\
+Content-Type: text/html\r\n\r\n" 
++ getindex()), boost::asio::transfer_all(), err);
+		
+	}
 }
