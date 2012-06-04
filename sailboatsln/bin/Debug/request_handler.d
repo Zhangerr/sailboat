bee45b82c3888708d639e4b8f2423d49c6f21382
alex zhang <alexzhangh@gmail.com>
2012-06-03 19:18:55 -0700

    structure cleanup

--- sailboatsln/bin/Debug/request_handler.d
+++
@@ -1,5 +0,0 @@
-/home/alex/sailboatsln/sailboatsln/bin/Debug/request_handler.o: \
- /home/alex/sailboatsln/sailboatsln/request_handler.cpp \
- /home/alex/sailboatsln/sailboatsln/request_handler.hpp \
- /home/alex/sailboatsln/sailboatsln/response.hpp \
- /home/alex/sailboatsln/sailboatsln/request.hpp
--- sailboatsln/bin/Debug/test.d
+++
@@ -1,5 +0,0 @@
-/home/alex/sailboatsln/sailboatsln/bin/Debug/test.o: \
- /home/alex/sailboatsln/sailboatsln/test.cpp \
- /home/alex/sailboatsln/sailboatsln/request_handler.hpp \
- /home/alex/sailboatsln/sailboatsln/response.hpp \
- /home/alex/sailboatsln/sailboatsln/request.hpp
