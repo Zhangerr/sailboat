1ee6a4cf466cdff463d96eb9ae8678051f26ae2e
alex zhang <alexzhangh@gmail.com>
2012-06-19 22:47:54 -0700

    url decode function

--- sailboatsln/Util.cpp
+++ sailboatsln/Util.cpp
@@ -195,6 +195,46 @@ namespace Util {
 	bool isLuaFile(string name) {
 		return boost::ends_with(name,".lua");
 	}
+	//http://www.boost.org/doc/libs/1_40_0/doc/html/boost_asio/example/http/server3/request_handler.cpp
+	bool url_decode(string in, string& out)
+	{
+	  out.clear();
+	  out.reserve(in.size());
+	  for (std::size_t i = 0; i < in.size(); ++i)
+	  {
+		if (in[i] == '%')
+		{
+		  if (i + 3 <= in.size())
+		  {
+			int value = 0;
+			std::istringstream is(in.substr(i + 1, 2));
+			if (is >> std::hex >> value)
+			{
+			  out += static_cast<char>(value);
+			  i += 2;
+			}
+			else
+			{
+			  return false;
+			}
+		  }
+		  else
+		  {
+			return false;
+		  }
+		}
+		else if (in[i] == '+')
+		{
+		  out += ' ';
+		}
+		else
+		{
+		  out += in[i];
+		}
+	  }
+	  return true;
+	}
+
 	bool loadMime() {
 		//string s = getFile("/etc/mime.types"); //this could just be converted to a static map
 		if(!exists("mime.types")) {
