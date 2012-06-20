96e817d16fd0b518e4c7893529684390d105813b
alex zhang <alexzhangh@gmail.com>
2012-06-19 18:16:13 -0700

    added some error checking to parseXML, consider using Doxygen for documentation generation?

--- sailboatsln/main.cpp
+++ sailboatsln/main.cpp
@@ -17,29 +17,10 @@ using boost::asio::ip::tcp;
 //need to include content-length but it seems it is not sent by all servers
 //possible todos -- gets posts cookies
 //lua design -- go php style & just have globals with post/get params? or pass those as arguments (varargs or table)
+//perhaps generate doxygen documentation?, use asserts
 int main()
 {
 	
-/*	CLuaVirtualMachine vm;
-    vm.InitialiseVM ();
-	//vm.RunFile("test.lua");
-	//vm.RunFile("test1.lua");
-	//vm.DumpStack();
-	
-	   CMyScript ms (vm);
-	  // cout << ms.getI() << endl;
-	  // getchar();
-	 //  return 0;
-	 //  ms.getBuffer();
-   ms.CompileFile ("test.lua");
-      ms.SelectScriptFunction ("test");
-   ms.AddParam ("asd");
-   ms.AddParam("vds");
-   ms.AddParam("testo");
-   ms.AddParam("plsworko");
-   ms.AddParam(2.3f);
-   ms.Go ();
-      vm.DumpStack();*/
 	if(!Util::loadMime()) {
 		cerr << "Please make sure you have mimes.type in the working directory" << endl;
 		return -1;
